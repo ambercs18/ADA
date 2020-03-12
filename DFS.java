88b5542666d833e20a7866665eaf799e276efead
ambercs18 <53806443+ambercs18@users.noreply.github.com>
2020-03-12 11:47:40 +0530

    Update DFS.java

--- DFS.java
+++ DFS.java
@@ -2,12 +2,8 @@ import java.util.*;
 public class DFS {
 	static int ar[][];
 	static boolean v[];
-	static ArrayList<Integer> l=new ArrayList<>();
+	//static ArrayList<Integer> l=new ArrayList<>();
 	static int f=0;
-	public static int[] getAdj(int v)
-	{
-		return ar[v];
-	}
 	public static void main(String a[])
 	{
 		Scanner x=new Scanner(System.in);
@@ -23,11 +19,8 @@ public class DFS {
 		for(int i=0;i<n;i++)
 			v[i]=false;
 		dfs(0);
-        if(f==0)
-        	System.out.println("Cycle is Absent");
-        else
-        	System.out.println(f+" Cycles are Present");
-		/*int c=0,p=0;
+ 
+		int c=0,p=0;
 		for(int i=0;i<n;i++) {
 			c=0;
 			for(int j=0;j<n;j++) {
@@ -41,20 +34,18 @@ public class DFS {
 		if(p==1) {
 			System.out.println("disconnected");
 		}
-		else System.out.println("connected");*/
+		else System.out.println("connected");
 		
 	}
 	
 	public static void dfs(int i)
 	{
 		int j;
-		l.add(i);
+		System.out.println(i);
 		v[i]=true;
 		for(j=0;j<v.length;j++)
 		{
-			 if(v[j]&&ar[i][j]==1&&l.contains(j))
-		    	   f++;
-			 else if(!v[j]&&ar[i][j]==1)
+	                   if(!v[j]&&ar[i][j]==1)
 		            dfs(j);
 		      
 		}
